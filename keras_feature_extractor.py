914f3d19c28daaa3606b0b066e64aa2f6bf0f8a1
Samuel Michalik <samuelmichalik@yahoo.com>
2019-06-13 15:05:40 +0200

    add input size attribute to KerasEncoderSpec

--- keras_feature_extractor.py
+++ keras_feature_extractor.py
@@ -7,21 +7,21 @@ import numpy as np
 
 from feature_extractor import FeatureExtractor
 
-S = namedtuple('KerasEncoderSpec', ['net', 'module'])
+S = namedtuple('KerasEncoderSpec', ['net', 'module', 'input_size'])
 
 MODELS = {
-    'Xception': S('Xception', 'xception'),
-    'VGG16': S('VGG16', 'vgg16'),
-    'VGG19': S('VGG19', 'vgg19'),
-    'ResNet50': S('ResNet50', 'resnet'),
-    'ResNet101': S('ResNet101', 'resnet'),
-    'ResNet152': S('ResNet152', 'resnet'),
-    'ResNet50V2': S('ResNet50V2', 'resnet_v2'),
-    'ResNet101V2': S('ResNet101V2', 'resnet_v2'),
-    'ResNet152V2': S('ResNet152V2', 'resnet_v2'),
-    'ResNeXt50': S('ResNetXt50', 'resnext'),
-    'ResNeXt101': S('ResNeXt101', 'resnext'),
-    'InceptionV3': S('InceptionV3', 'inception_v3')
+    'Xception': S('Xception', 'xception', (299, 299)),
+    'VGG16': S('VGG16', 'vgg16', (224, 224)),
+    'VGG19': S('VGG19', 'vgg19', (224, 224)),
+    'ResNet50': S('ResNet50', 'resnet', (224, 224)),
+    'ResNet101': S('ResNet101', 'resnet', (224, 224)),
+    'ResNet152': S('ResNet152', 'resnet', (224, 224)),
+    'ResNet50V2': S('ResNet50V2', 'resnet_v2', (224, 224)),
+    'ResNet101V2': S('ResNet101V2', 'resnet_v2', (224, 224)),
+    'ResNet152V2': S('ResNet152V2', 'resnet_v2', (224, 224)),
+    'ResNeXt50': S('ResNetXt50', 'resnext', (224, 224)),
+    'ResNeXt101': S('ResNeXt101', 'resnext', (224, 224)),
+    'InceptionV3': S('InceptionV3', 'inception_v3', (299, 299))
 }
 
 class KerasFeatureExtractor(FeatureExtractor):
@@ -34,15 +34,14 @@ class KerasFeatureExtractor(FeatureExtractor):
         model_constr = getattr(module, enc_spec.net) 
         self._model = model_constr(weights='imagenet', include_top=False, pooling=None)
         self._preprocess_input = getattr(module, 'preprocess_input')
-        self._width = width
-        self._height = height
+        self._input_size = enc_spec.input_size
 
     def extract_features(self, dataset):
         prefix = dataset.prefix
         elems = dataset.elements
         paths = [os.path.join(prefix, e.source) for e in elems]
 
-        imgs = [image.load_img(p, target_size=(self._width, self._height)) 
+        imgs = [image.load_img(p, target_size=self._input_size) 
             for p in paths]
         xs = [image.img_to_array(i) for i in imgs]
         xs = [np.expand_dims(x, axis=0) for x in xs]
