73f547e1e2a830fdabb8a48ff490060f49fc57aa
Samuel Michalik <samuelmichalik@yahoo.com>
2019-06-28 16:40:21 +0200

    untracking files

--- config.py
+++
@@ -1,8 +0,0 @@
-import interface
-
-class Configuration:
-    def __init__(self, args=None):
-        pass
-
-    def create_model_interface(self):
-        return interface.NeuralMonkeyModelInterface()
--- feature_extractor.py
+++
@@ -1,6 +0,0 @@
-class FeatureExtractor():
-    def __init__(self):
-        pass
-
-    def extract_features(self, dataset):
-        pass
