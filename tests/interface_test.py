46fb461afb682beaacb96b93c4ba20e1ce8b5d42
Samuel Michalik <samuelmichalik@yahoo.com>
2019-07-10 12:38:50 +0200

    rename variable to adhere to convention

--- tests/interface_test.py
+++ tests/interface_test.py
@@ -3,19 +3,19 @@ import json
 from interface import create_model_interface, ModelInterface
 from data import Dataset
 
-JSON_CONFIG = "./tests/data/model.json"
+TEST_CONFIG = "./tests/data/model.json"
 
 TEST_PREFIX = "./tests/data/flickr8k_sample_imgs"
 TEST_SOURCES = "./tests/data/flickr8k_sample_imgs.txt"
 TEST_NAME = "test_ds"
 
 def test_interface_create_model_interface():
-    json_data = json.load(open(JSON_CONFIG))
+    json_data = json.load(open(TEST_CONFIG))
     ifc = create_model_interface(json_data)
     assert isinstance(ifc, ModelInterface) == True
 
 def test_interface_run_model_on_dataset():
-    json_model = json.load(open(JSON_CONFIG))
+    json_model = json.load(open(TEST_CONFIG))
     model = create_model_interface(json_model)
 
     ds = Dataset(name=TEST_NAME,
