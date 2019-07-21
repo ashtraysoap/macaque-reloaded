4883cedec31f3969b0f37537aed184043f98341b
Samuel Michalik <samuelmichalik@yahoo.com>
2019-07-21 15:20:09 +0200

    add InformativeInput

--- frontend/aboutTab.js
+++ frontend/aboutTab.js
@@ -5,6 +5,7 @@ function AboutTab() {
         <div>
             <h1>Macaque</h1>
             <p>An application for building intuitions about neural network inference and evaluation.</p>
+            <i>Created by Samuel Michalik</i>
         </div>
     )
 }
--- frontend/addDatasetTab.js
+++ frontend/addDatasetTab.js
@@ -1,8 +1,41 @@
-import { MockTab } from './mockTab.js';
+import React from 'react';
+
+import { InformativeInput } from './utils.js';
 
 
-function AddDatasetTab() {
-    return MockTab();
-}
+class AddDatasetTab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            prefix: "",
+            sources: "",
+            references: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(k, v) {
+        let o = {};
+        o[k] = v;
+        this.setState(o);
+    }
+
+    render() {
+        const s = this.state;
+
+        return (
+            <div>
+                <InformativeInput
+                    name="dataset name"
+                    value={s.name}
+                    optional={false}
+                    hint="A name for the dataset."
+                    handleChange={(e) => this.handleChange("name", e.target.value)}
+                />
+            </div>
+        );
+    }
+} 
 
 export { AddDatasetTab };
