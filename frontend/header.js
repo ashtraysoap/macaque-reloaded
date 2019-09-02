a1ed5cdc023ddf8aecaacc3a4954617438fbeae0
Samuel Michalik <samuelmichalik@yahoo.com>
2019-09-02 13:49:34 +0200

    add header component

---
+++ frontend/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+function Header() {
+    return (
+        <div class="header">
+            <h1>Macaque</h1>
+        </div>
+    )
+}
+
+export { Header };
