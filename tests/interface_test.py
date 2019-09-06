4d3336f438cc9afc1fc64d4a95df09208cb0c155
Samuel Michalik <samuelmichalik@yahoo.com>
2019-09-06 15:39:25 +0200

    improve model interface testing

--- tests/interface_test.py
+++ tests/interface_test.py
@@ -2,8 +2,10 @@ import json
 
 from interface import create_model_interface, ModelInterface
 from data import Dataset
+from tests.data_test import make_initialized_test_dataset
 
 TEST_CONFIG = "./tests/data/model.json"
+MOCK_PLUGIN_MODEL = "./tests/data/mock_plugin_model.json"
 
 TEST_PREFIX = "./tests/data/flickr8k_sample_imgs"
 TEST_SOURCES = "./tests/data/flickr8k_sample_imgs.txt"
@@ -14,14 +16,16 @@ def test_interface_create_model_interface():
     ifc = create_model_interface(json_data)
     assert isinstance(ifc, ModelInterface) == True
 
-def test_interface_run_model_on_dataset():
+def test_interface_run_on_dataset():
     json_model = json.load(open(TEST_CONFIG))
     model = create_model_interface(json_model)
+    ds = make_initialized_test_dataset()
+    res = model.run_on_dataset(ds)
+    assert res != None
 
-    ds = Dataset(name=TEST_NAME,
-                prefix=TEST_PREFIX,
-                batch_size=8)
-    ds.initialize(fp=TEST_SOURCES)
-
+def test_interface_run_on_dataset_mock_plugin_model():
+    json_model = json.load(open(MOCK_PLUGIN_MODEL))
+    model = create_model_interface(json_model)
+    ds = make_initialized_test_dataset()
     res = model.run_on_dataset(ds)
     assert res != None
---
+++ tests/mock_plugin_model.py
@@ -0,0 +1,11 @@
+class ModelWrapper:
+    def run_on_paths(self, paths):
+        print("Entering mock model wrapper.")
+        x = { 
+            'caption': "Colorless green ideas sleep furiously",
+            'alignments': None,
+            'beam_search_output': None
+        }
+        res = [x for p in paths]
+        print("Computation complete, returning mock resuslts.")
+        return res
