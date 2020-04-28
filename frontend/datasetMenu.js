5860e46edf93233957a8e9e5a2c098f7e10a8296
Samuel Michalik <samuelmichalik@yahoo.com>
2020-04-28 15:40:18 +0200

    pending tab shows while processing dataset

--- frontend/datasetMenu.js
+++ frontend/datasetMenu.js
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import { MultipleSelectionWithButton, range } from './utils.js';
+import { PendingTab } from './statusTabs.js';
+
 
 export { DatasetMenu };
 
@@ -27,29 +29,42 @@ class RunModelsTab extends React.Component {
     constructor(props) {
         super(props);
         this.runOnDataset = this.runOnDataset.bind(this);
+        this.state = { processing: 0 };
     }
 
     runOnDataset(runnerIds) {
         runnerIds.forEach(runner => {
+            this.setState({ processing: this.state.processing + 1 });
+
             fetch(`/run_on_dataset/${this.props.datasetId}/${runner}`)
             .then(res => res.json())
             .then(response => {
                 console.log('Success:', JSON.stringify(response));
+                this.setState({ processing: this.state.processing - 1 });
                 this.props.onResultsResponse(response);
             })
-            .catch(error => console.log('Error:', error));
+            .catch(error => {
+                console.log('Error:', error);
+                this.setState({ processing: this.state.processing - 1 });
+            });
         });
     }
 
     render() {
+        let statusTab = this.state.processing > 0 ? <PendingTab text="Processing."/> : null;
+
         return (
-            <MultipleSelectionWithButton
-                label="Inference"
-                keys={range(this.props.runnerNames.length)}
-                values={this.props.runnerNames}
-                onSubmit={this.runOnDataset}
-                buttonText="run"
-            />
+            <div>
+                <MultipleSelectionWithButton
+                    label="Inference"
+                    keys={range(this.props.runnerNames.length)}
+                    values={this.props.runnerNames}
+                    onSubmit={this.runOnDataset}
+                    buttonText="run"
+                />
+                { statusTab }
+            </div>
+            
         );
     }
 }
