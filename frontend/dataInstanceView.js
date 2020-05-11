804f24fe1ace08b0d108d956374cd18a21395125
Samuel Michalik <samuelmichalik@yahoo.com>
2020-05-12 01:27:08 +0200

    style improvements

--- frontend/dataInstanceView.js
+++ frontend/dataInstanceView.js
@@ -20,7 +20,6 @@ class DataInstanceView extends React.Component {
         this.onKeyDown = this.onKeyDown.bind(this);
 
         //this.imgSrc = `/load_image/${this.props.dataset}/${this.props.dataInstance.id}`;
-        this.runId = (props.results.length === 0) ? null : this.props.results[0].runId;
         this.state = { 
             imgSrc: this.imgSrc,
             tokenId: null,
@@ -31,36 +30,59 @@ class DataInstanceView extends React.Component {
         return `/load_image/${this.props.dataset}/${this.props.dataInstance.id}`;
     }
 
+    get selectedResults() {
+        const rid = this.selectedRunId;
+        if (rid === null)
+            return null;
+        return this.props.results.filter(r => r.runId === rid)[0];
+    }
+
+    get selectedRunId() {
+        const r = this.props.results;
+        if (r === undefined || r === null || r.length === 0)
+            return null;
+        return r[0].runId;
+    }
+
     render() {
         const instance = this.props.dataInstance;
         const results = this.props.results;
 
-        if (this.runId === null && results.length > 0) {
-            this.runId = results[0].runId;
+        console.log(results);
+        if (results === null || results === undefined || results.length === 0) {
+            return (
+                <div className="transparentLayer" onClick={() => this.props.onClick()}>
+                    <div className="instanceViewNoResults" id="X" onClick={(e) => e.stopPropagation()} onKeyDown={e => this.onKeyDown(e)} tabIndex="0">
+                        <div className="staticLabel">
+                            { basename(instance.source) }
+                        </div>
+                        <div>
+                            <img src={this.state.imgSrc} className="homeTabImg" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            )
         }
-
-        // results from the selected run
-        const selectedRes = (this.runId === null) ? null : results.filter(r => r.runId === this.runId)[0];
-        const runResultsView = (this.runId === null) ? null : <RunResultsView 
-            results={selectedRes.results} 
-            instanceId={instance.id}
-            runId={this.runId}
-            onCaptionClick={this.onCaptionClick}
-            fetchAttentionMap={this.fetchAttentionMap}
-            fetchAttentionMapForBSToken={this.fetchAttentionMapForBSToken}
-        />;
 
         return (
             <div className="transparentLayer" onClick={() => this.props.onClick()}>
                 <div className="instanceView" id="X" onClick={(e) => e.stopPropagation()} onKeyDown={e => this.onKeyDown(e)} tabIndex="0">
-                    
-                    { basename(instance.source) }
-                    
-                    <div>
-                        <img src={this.state.imgSrc} alt=""/>
+                    <div className="instanceInfo">
+                        { basename(instance.source) }
+                        <div>
+                            <img src={this.state.imgSrc} className="homeTabImg" alt=""/>
+                        </div>
                     </div>
-
-                    {runResultsView}
+                    <div>
+                        <RunResultsView 
+                            results={this.selectedResults.results} 
+                            instanceId={instance.id}
+                            runId={this.selectedRunId}
+                            onCaptionClick={this.onCaptionClick}
+                            fetchAttentionMap={this.fetchAttentionMap}
+                            fetchAttentionMapForBSToken={this.fetchAttentionMapForBSToken}
+                        />;
+                    </div>
                 </div>
             </div>
         );
@@ -141,6 +163,10 @@ class DataInstanceView extends React.Component {
             const newIdx = this.props.onInstanceChange(key);
             this.setState({ imgSrc: `/load_image/${this.props.dataset}/${newIdx}` });
         }
+        // ESC key or backspace pressed:
+        if (key == "27" || key == "8") {
+            // close tab
+        }            
     }
 }
 
