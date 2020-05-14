415973c8b15187ea57ace955569d4f245e49d0b3
Samuel Michalik <samuelmichalik@yahoo.com>
2020-05-14 14:20:09 +0200

    untrack deleted files

--- frontend/addSomethingTab.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export { AddSomethingTab };
-
-const AddSomethingTab = (props) => (
-    <div className="addSomethingTab">
-        {props.children}
-    </div>
-);
--- lib/neuralmonkey/neuralmonkey/attention/__init__.py
+++
@@ -1,3 +0,0 @@
-from .feed_forward import Attention
-from .coverage import CoverageAttention
-from .scaled_dot_product import ScaledDotProdAttention
