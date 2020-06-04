45839f0e3909230cad79edfbfcd123fed4986bf7
Samuel Michalik <samuelmichalik@yahoo.com>
2020-06-04 15:27:45 +0200

    comment out several tests due to SIS size limitations on the attachments

--- tests/feature_extractors/neural_monkey_feature_extractor_test.py
+++ tests/feature_extractors/neural_monkey_feature_extractor_test.py
@@ -11,27 +11,34 @@ TEST_SLIM_PATH = "lib/slim/"
 TEST_MODEL_CKPT = "../models/plugin_resources/vgg_19.ckpt"
 TEST_CONV_MAP = "vgg_19/conv5/conv5_3"
 
-def create_neural_monkey_feature_extractor():
-    return NeuralMonkeyFeatureExtractor(
-        net=TEST_NET_ID,
-        slim_models=TEST_SLIM_PATH,
-        model_checkpoint=TEST_MODEL_CKPT,
-        conv_map=TEST_CONV_MAP)
+#   Note: 
+#
+#   Tests are commented out because they rely on model
+#   checkpoints that were too large for SIS's size
+#   restrictions for the thesis attachments of 850MB.
 
-def test_neural_monkey_feature_extractor_constructor():
-    with tf.Graph().as_default():
-        ext = create_neural_monkey_feature_extractor()
-        assert isinstance(ext, NeuralMonkeyFeatureExtractor) == True
 
-def test_neural_monkey_feature_extractor_extract_features():
-    with tf.Graph().as_default():
-        ext = create_neural_monkey_feature_extractor()
-        ds = make_initialized_test_dataset()
-        ds.load_images()
-        imgs = [e.image for e in ds.elements]
-        prepro = Preprocessor()
-        imgs = prepro.preprocess_images(imgs)
+# def create_neural_monkey_feature_extractor():
+#     return NeuralMonkeyFeatureExtractor(
+#         net=TEST_NET_ID,
+#         slim_models=TEST_SLIM_PATH,
+#         model_checkpoint=TEST_MODEL_CKPT,
+#         conv_map=TEST_CONV_MAP)
 
-        results = ext.extract_features(images=imgs)
-        assert isinstance(results, np.ndarray) == True
-        assert results.shape[0] == ds.count
+# def test_neural_monkey_feature_extractor_constructor():
+#     with tf.Graph().as_default():
+#         ext = create_neural_monkey_feature_extractor()
+#         assert isinstance(ext, NeuralMonkeyFeatureExtractor) == True
+
+# def test_neural_monkey_feature_extractor_extract_features():
+#     with tf.Graph().as_default():
+#         ext = create_neural_monkey_feature_extractor()
+#         ds = make_initialized_test_dataset()
+#         ds.load_images()
+#         imgs = [e.image for e in ds.elements]
+#         prepro = Preprocessor()
+#         imgs = prepro.preprocess_images(imgs)
+
+#         results = ext.extract_features(images=imgs)
+#         assert isinstance(results, np.ndarray) == True
+#         assert results.shape[0] == ds.count
--- tests/model_wrappers/neural_monkey_model_wrapper_test.py
+++ tests/model_wrappers/neural_monkey_model_wrapper_test.py
@@ -6,48 +6,55 @@ from model_wrappers import NeuralMonkeyModelWrapper
 TEST_PREFIX = "../tests/data/flickr8k_sample_feats"
 TEST_SOURCES = "../tests/data/flickr8k_sample_feats.txt"
 
-def create_neural_monkey_model_wrapper_enc_dec():
-    return NeuralMonkeyModelWrapper(
-        config_path="../models/plugin_resources/enc-dec-test/enc-dec-test.ini",
-        vars_path="../models/plugin_resources/enc-dec-test/variables.data.final",
-        runs_on_features=False,
-        data_series="images")
+#   Note: 
+#
+#   Tests are commented out because they rely on model
+#   checkpoints that were too large for SIS's size
+#   restrictions for the thesis attachments of 850MB.
 
-def create_neural_monkey_model_wrapper_dec_only():
-    return NeuralMonkeyModelWrapper(
-        config_path="../tests/data/dec-only.ini",
-        vars_path="../tests/data/output/dec-only-test/variables.data.final",
-        runs_on_features=True,
-        data_series="feature_maps",
-        caption_series="greedy_caption")
 
-def test_neural_monkey_model_wrapper_constructor_enc_dec():
-    model = create_neural_monkey_model_wrapper_enc_dec()
-    assert isinstance(model, NeuralMonkeyModelWrapper)
+# def create_neural_monkey_model_wrapper_enc_dec():
+#     return NeuralMonkeyModelWrapper(
+#         config_path="../models/plugin_resources/enc-dec-test/enc-dec-test.ini",
+#         vars_path="../models/plugin_resources/enc-dec-test/variables.data.final",
+#         runs_on_features=False,
+#         data_series="images")
 
-def test_neural_monkey_model_wrapper_constructor_dec_only():
-    model = create_neural_monkey_model_wrapper_dec_only()
-    assert isinstance(model, NeuralMonkeyModelWrapper)
+# def create_neural_monkey_model_wrapper_dec_only():
+#     return NeuralMonkeyModelWrapper(
+#         config_path="../tests/data/dec-only.ini",
+#         vars_path="../tests/data/output/dec-only-test/variables.data.final",
+#         runs_on_features=True,
+#         data_series="feature_maps",
+#         caption_series="greedy_caption")
 
-def test_neural_monkey_model_wrapper_enc_dec_run():
-    model = create_neural_monkey_model_wrapper_enc_dec()
-    ds = make_initialized_test_dataset()
-    ds.load_images()
-    imgs = [e.image for e in ds.elements]
-    from preprocessing import Preprocessor
-    prepro = Preprocessor()
-    imgs = prepro.preprocess_images(imgs)
-    results = model.run(imgs)
-    assert isinstance(results, list) == True
-    assert len(results) == ds.count
-    assert isinstance(results[-1], dict) == True
+# def test_neural_monkey_model_wrapper_constructor_enc_dec():
+#     model = create_neural_monkey_model_wrapper_enc_dec()
+#     assert isinstance(model, NeuralMonkeyModelWrapper)
 
-def test_neural_monkey_model_wrapper_dec_only_run():
-    model = create_neural_monkey_model_wrapper_dec_only()
-    ds = make_initialized_test_dataset()
-    ds.attach_features_from_file_list(prefix=TEST_PREFIX, sources=TEST_SOURCES)
-    features = [e.feature_map for e in ds.elements]
-    results = model.run(np.asarray(features))
-    assert isinstance(results, list) == True
-    assert len(results) == ds.count
-    assert isinstance(results[-1], dict) == True
+# def test_neural_monkey_model_wrapper_constructor_dec_only():
+#     model = create_neural_monkey_model_wrapper_dec_only()
+#     assert isinstance(model, NeuralMonkeyModelWrapper)
+
+# def test_neural_monkey_model_wrapper_enc_dec_run():
+#     model = create_neural_monkey_model_wrapper_enc_dec()
+#     ds = make_initialized_test_dataset()
+#     ds.load_images()
+#     imgs = [e.image for e in ds.elements]
+#     from preprocessing import Preprocessor
+#     prepro = Preprocessor()
+#     imgs = prepro.preprocess_images(imgs)
+#     results = model.run(imgs)
+#     assert isinstance(results, list) == True
+#     assert len(results) == ds.count
+#     assert isinstance(results[-1], dict) == True
+
+# def test_neural_monkey_model_wrapper_dec_only_run():
+#     model = create_neural_monkey_model_wrapper_dec_only()
+#     ds = make_initialized_test_dataset()
+#     ds.attach_features_from_file_list(prefix=TEST_PREFIX, sources=TEST_SOURCES)
+#     features = [e.feature_map for e in ds.elements]
+#     results = model.run(np.asarray(features))
+#     assert isinstance(results, list) == True
+#     assert len(results) == ds.count
+#     assert isinstance(results[-1], dict) == True
